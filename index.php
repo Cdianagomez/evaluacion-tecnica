ff1f939b1ada7b8b4271934c1361f2c72f5140c9
Cdianagomez <95441763+Cdianagomez@users.noreply.github.com>
2022-10-27 14:11:13 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    
+    <body>
+        <?php
+        function espacios($espacio){
+            for($i=0; $i<2*$espacio-1; $i++){
+                echo "&nbsp";
+            }
+        }
+        
+        function ceros($espacio){
+            for($i=0; $i<2*$espacio-1; $i++){
+                echo "0";
+            }
+        }
+        echo "&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp"."*";
+        for($espacio=0; $espacio<10; $espacio=$espacio+1){
+            espacios(10-$espacio);
+            ceros($espacio);
+            espacios(10-$espacio);
+            echo "<br>";
+        }
+        ?>
+    </body>
+</html>
